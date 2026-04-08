73cbfa9a71808cc7e1cbe7dc25ce8148a16599be
Aswathi <aswathiaj23@gmail.com>
2026-04-08 20:15:50 +0530

    modified api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,10 @@
-// client/src/services/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Adjust to your backend URL
+  baseURL: "http://localhost:5000/api",
 });
 
-// Add Authorization header automatically
+// Add token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
