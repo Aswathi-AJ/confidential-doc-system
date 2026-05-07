02efd293f82b9329b4b5ba15f7338494e71567f2
Aswathi <aswathiaj23@gmail.com>
2026-05-07 09:53:09 +0530

    modified changes for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST || process.env.DB_HOST,
-  user: process.env.MYSQLUSER || process.env.DB_USER,
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || 3306
+  host: process.env.MYSQL_HOST || process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQL_USER || process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQL_PORT || process.env.MYSQLPORT || 3306
 });
 
 db.connect((err) => {
